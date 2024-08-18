4809d7ea8c95d9f6144532865654fac3cc255d67
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-08-18 19:15:34 +0100

    new shii

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,7 @@ const [count, setCount] = useState(0)
     <h1 className="bg-gray-400 text-cyan-300 text-center h-24  rounded-3xl ml-6 mr-6 mt-6 p-5 border-slate-900 border-2">{advice}</h1>
     <button onClick={getAdvice} className="bg-slate-800 rounded-xl text-cyan-200 w-32 mt-3 mb-3 p-3 border-2 border-slate-400">More Advice</button>  
     <Message count={count}/>
+    <h3 className="mb-0 pb-0 mt-48">By Abdullahi Olaiwon</h3>
   </div>
   )
   
